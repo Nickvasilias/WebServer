10df8d6c0f2b62dc1c0d75b1e770b8e8c500c273
Nikos Vasilikopoulos <provatogames@gmail.com>
2020-03-03 20:23:13 +0200

    Delete temp.js

--- temp.js
+++
@@ -1,7 +0,0 @@
-app.get('/SubmitQuestionnaire/:patient_id/:answer1/:answer2', submitQuestionnaire)
-app.get('/getPatients/:doctor_username', getPatients)
-app.get('/GetPatientHoursAwake/:patient_id/:startDate/:endDate', getPatientHoursAwake)
-app.get('/GetPatientIsWellHours/:patient_id/:startDate/:endDate', getPatientIsWellHours)
-app.get('/GetPatientNotWalkingHours/:patient_id/:startDate/:endDate', getPatientNotWalkingHours)
-app.get('/GetPatientSlowAndDifficultWalkingHours/:patient_id/:startDate/:endDate', getPatientSlowAndDifficultWalkingHours)
-app.get('/GetPatientHyperactivityHours/:patient_id/:startDate/:endDate', getPatientHyperactivityHours)
